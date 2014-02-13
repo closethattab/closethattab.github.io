3c54944204a976ef8ff453db54d2967789a30e9e
Robert Dawson <rob@sumzero.com>
2014-02-12 20:44:01 -0500

    test

--- choices.js
+++ choices.js
@@ -5,5 +5,5 @@ Array.prototype.shuffle = function () { //v1.0
 
 window.activities = 'take a walk, reread a good book, stare at a wall, try a new tea, type gibberish into a text file, doodle a rabbit, find a dog and pet it, make a nice sandwich, smell a candle, think about penguins, image search "marmoset", turn off the internet, bake some cookies, learn to finger knit, memorize a poem, go outside for like three goddamn minutes'.split(", ").shuffle();
 
-window.colors = "#FFFEFA".split(", ").shuffle()
+window.colors = "#444".split(", ").shuffle()
 
